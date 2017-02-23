03472fb7f3cc35dbadeeeb1b390f1b23d892a855
Patrick Bellasi <patrick.bellasi@arm.com>
2017-02-23 11:04:19 +0000

    example of possible improved formatting?
    If you like it it should be done for other *_energy.py modules as well ;-)

--- libs/utils/platforms/hikey_energy.py
+++ libs/utils/platforms/hikey_energy.py
@@ -20,28 +20,36 @@ from energy_model import (ActiveState, EnergyModelNode, EnergyModelRoot,
 from collections import OrderedDict
 
 cluster_active_states = OrderedDict([
-    (208000, ActiveState(capacity=178, power=16)),
-    (432000, ActiveState(capacity=369, power=29)),
-    (729000, ActiveState(capacity=622, power=47)),
-    (960000, ActiveState(capacity=819, power=75)),
-    (1200000, ActiveState(capacity=1024, power=112))
+#   ( frequency,             capacity, power)),
+    (    208000, ActiveState(     178,    16)),
+    (    432000, ActiveState(     369,    29)),
+    (    729000, ActiveState(     622,    47)),
+    (    960000, ActiveState(     819,    75)),
+    (   1200000, ActiveState(    1024,   112))
 ])
 
 cluster_idle_states = OrderedDict([
-    ('WFI', 47),
-    ('cpu-sleep', 47),
-    ('cluster-sleep', 0)
+#   (           name, power),
+    (          'WFI',    47),
+    (    'cpu-sleep',    47),
+    ('cluster-sleep',     0)
 ])
 
 cpu_active_states = OrderedDict([
-    (208000,  ActiveState(capacity=178,  power=69)),
-    (432000,  ActiveState(capacity=369,  power=125)),
-    (729000,  ActiveState(capacity=622,  power=224)),
-    (960000,  ActiveState(capacity=819,  power=367)),
-    (1200000, ActiveState(capacity=1024, power=670))
+#   ( frequency,             capacity, power)),
+    (    208000, ActiveState(     178,    69)),
+    (    432000, ActiveState(     369,   125)),
+    (    729000, ActiveState(     622,   224)),
+    (    960000, ActiveState(     819,   367)),
+    (   1200000, ActiveState(    1024,   670))
 ])
 
-cpu_idle_states = OrderedDict([('WFI', 15), ('cpu-sleep', 0), ('cluster-sleep', 0)])
+cpu_idle_states = OrderedDict([
+#   (           name, power),
+    (          'WFI',    15),
+    (    'cpu-sleep',     0),
+    ('cluster-sleep',     0)
+])
 
 def cpu_pd(cpu):
     return PowerDomain(cpu=cpu, idle_states=['WFI', 'cpu-sleep'])
@@ -59,10 +67,16 @@ hikey_energy = EnergyModel(
         EnergyModelNode(name='cluster1',
                         children=[cpu_node(c) for c in [4, 5, 6, 7]],
                         active_states=cluster_active_states,
-                        idle_states=cluster_idle_states)]),
-    root_power_domain=PowerDomain(idle_states=[], children=[
-        PowerDomain(idle_states=["cluster-sleep"], children=[
-            cpu_pd(c) for c in [0, 1, 2, 3]]),
-        PowerDomain(idle_states=["cluster-sleep"], children=[
-            cpu_pd(c) for c in [4, 5, 6, 7]])]),
-    freq_domains=[[0, 1, 2, 3, 4, 5, 6, 7]])
+                        idle_states=cluster_idle_states)
+    ]),
+    root_power_domain=PowerDomain(children=[
+        PowerDomain(idle_states=["cluster-sleep"],
+                    children=[cpu_pd(c) for c in [0, 1, 2, 3]]),
+        PowerDomain(idle_states=["cluster-sleep"],
+                    children=[cpu_pd(c) for c in [4, 5, 6, 7]])
+        ],
+        idle_states=[],
+    ),
+    freq_domains=[
+        [0, 1, 2, 3, 4, 5, 6, 7]
+    ])
